34d668abd5876ced22c5fd10b4c6b4ff2347d3f3
thanawit <thanawit@gmail.com>
2019-04-29 15:34:51 +0700

    font+bg

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,17 +6,43 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
-  Widget showAppName(){
-    return Text('Show App Name');
+  Widget showAppName() {
+    return Text(
+      'Show App Name ',
+      style: TextStyle(
+          fontFamily: 'IndieFlower',
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.red),
+    );
+  }
+
+  Widget showLogo() {
+    return Image.asset('images/logo.png');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.pink[100], Colors.pink[300]],begin: Alignment(0, 1))),
         padding: EdgeInsets.only(top: 100.0),
         alignment: Alignment(0, -1),
-        child:showAppName(),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 200.0,
+              height: 200.0,
+              child: showLogo(),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 10.0),
+              child: showAppName(),
+            ),
+          ],
+        ),
       ),
     );
   }
