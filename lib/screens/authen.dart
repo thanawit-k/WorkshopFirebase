13a8813ac881e264451b70b78431c032b8e03aa8
thanawit <thanawit@gmail.com>
2019-04-29 14:09:40 +0700

    crete Method Return

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,8 +6,18 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showAppName(){
+    return Text('Show App Name');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('StatefulWidget');
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 100.0),
+        alignment: Alignment(0, -1),
+        child:showAppName(),
+      ),
+    );
   }
 }
